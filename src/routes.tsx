1e4f2e3370cb7e353abaf42842d90862d74a1cc1
Alex Alan Nunes <aalannunesdelima@apple.com>
2024-03-30 23:17:34 -0300

    refactor project haha

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/home";
-import { ColorViewPage } from "./pages/color-view";
-import { LayoutPage } from "./pages/layout";
 
 export function RoutesApp() {
   return (
     <Routes>
-      <Route element={<LayoutPage />}>
-        <Route index path="/" element={<HomePage />} />
-        <Route path=":colorShades" element={<HomePage />} />
-        <Route path="/color/:color" element={<ColorViewPage />} />
-      </Route>
+      <Route index path="/" element={<HomePage />} />
     </Routes>
   );
 }
