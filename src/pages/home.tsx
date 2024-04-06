5a623246cde12c97ee038caad888b676b45a2149
Alex Alan Nunes <aalannunesdelima@apple.com>
2024-04-06 00:44:35 -0300

    add url state

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,11 +15,41 @@ import {
   chakra,
   useToast,
 } from "@chakra-ui/react";
-import { BsStars } from "react-icons/bs";
-import { MdCheck, MdFavoriteBorder, MdSettingsSuggest } from "react-icons/md";
-import { Header } from "../components/header";
-import { ReactNode, useState } from "react";
 import chroma from "chroma-js";
+import { ReactNode, useState } from "react";
+import { BsStars } from "react-icons/bs";
+import {
+  MdCheck,
+  MdFavorite,
+  MdFavoriteBorder,
+  MdSettingsSuggest,
+} from "react-icons/md";
+import { useSearchParams } from "react-router-dom";
+import { useDebouncedCallback } from "use-debounce";
+import { Header } from "../components/header";
+
+const initialColors = [
+  {
+    id: 0,
+    color: "#48BB78",
+  },
+  {
+    id: 1,
+    color: "#4299e1",
+  },
+  {
+    id: 2,
+    color: "#0BC5EA",
+  },
+  {
+    id: 3,
+    color: "#F56565",
+  },
+  {
+    id: 4,
+    color: "#38B2AC",
+  },
+];
 
 function ColorBoxContainer({ children }: { children: ReactNode }) {
   return (
@@ -29,31 +59,43 @@ function ColorBoxContainer({ children }: { children: ReactNode }) {
   );
 }
 
-export function HomePage() {
-  const [colors, setColors] = useState([
-    {
-      id: 0,
-      color: "#48BB78",
-    },
-    {
-      id: 1,
-      color: "#4299e1",
-    },
-    {
-      id: 2,
-      color: "#0BC5EA",
-    },
-    {
-      id: 3,
-      color: "#F56565",
-    },
-    {
-      id: 4,
-      color: "#38B2AC",
-    },
-  ]);
+const convertColorsToString = (
+  colors: Array<{ id: number; color: string }>
+) => {
+  const colorsString = colors
+    .map((c) => c.color.replace("#", "").toLowerCase())
+    .join("-");
 
+  return colorsString;
+};
+
+function useColors() {
+  const [params] = useSearchParams();
+  const colorsParams = params.get("colors");
+
+  const [colors, setColors] = useState(() => {
+    if (colorsParams) {
+      // validate colors
+      return colorsParams
+        .split("-")
+        .filter((c) => !!c)
+        .map((color, id) => ({
+          color: `#${color}`,
+          id,
+        }));
+    }
+
+    return initialColors;
+  });
+
+  return { colors, setColors };
+}
+
+export function HomePage() {
+  const [, setSearchParams] = useSearchParams();
+  const { colors, setColors } = useColors();
   const toast = useToast();
+  const [saved, setSaved] = useState(false);
 
   const handleCopy = async (value: string) => {
     await navigator.clipboard.writeText(value);
@@ -86,9 +128,31 @@ export function HomePage() {
       ),
     });
   };
+
+  const updateUrlWithColors = useDebouncedCallback((newColors: string) => {
+    setSearchParams(
+      {
+        colors: newColors,
+      },
+      {
+        replace: true,
+      }
+    );
+  }, 800);
+
+  const handleNewColors = () => {
+    // set name
+
+    setColors(initialColors);
+    const urlColors = convertColorsToString(initialColors);
+
+    updateUrlWithColors(urlColors);
+  };
+
+  const templateGrid = `repeat(${colors.length}, 1fr)`;
   return (
     <Box minH={"100vh"}>
-      <Header />
+      <Header onClickNew={handleNewColors} />
       <Container maxW={"container.lg"} pt={10}>
         <Flex alignItems={"flex-end"} justifyContent={"space-between"} my={8}>
           <Stack spacing={0}>
@@ -107,12 +171,16 @@ export function HomePage() {
             <Button variant={"preLarge"} leftIcon={<MdSettingsSuggest />}>
               Adjust
             </Button>
-            <Button variant={"preLarge"} leftIcon={<MdFavoriteBorder />}>
+            <Button
+              variant={"preLarge"}
+              leftIcon={saved ? <MdFavorite /> : <MdFavoriteBorder />}
+              onClick={() => setSaved(!saved)}
+            >
               Saved
             </Button>
           </HStack>
         </Flex>
-        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        <Grid templateColumns={templateGrid} gap={6}>
           {colors.map(({ color, id }) => {
             const rgbColor = `rgb(${chroma(color).rgb().join(", ")})`;
             return (
@@ -144,17 +212,21 @@ export function HomePage() {
                     }}
                     value={color}
                     onChange={(e) => {
-                      setColors((prev) => {
-                        return prev.map((item) => {
-                          if (item.id === id) {
-                            return {
-                              ...item,
-                              color: e.target.value,
-                            };
-                          }
-                          return item;
-                        });
+                      const updatedColors = colors.map((item) => {
+                        if (item.id === id) {
+                          return {
+                            ...item,
+                            color: e.target.value,
+                          };
+                        }
+                        return item;
                       });
+
+                      setColors(updatedColors);
+
+                      const urlColors = convertColorsToString(updatedColors);
+
+                      updateUrlWithColors(urlColors);
                     }}
                   />
                   <Button
