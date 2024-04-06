1c03231ec504420c1a56fc6712cd744be5d30bb6
Alex Alan Nunes <aalannunesdelima@apple.com>
2024-04-06 00:44:31 -0300

    fix: button pressed does not scale down

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,16 +5,20 @@ export const Button = defineStyleConfig({
     fontWeight: "bold",
     transition: "transform .1s ease-in, background .2s ease-in",
     rounded: "xl",
+    "--pressed-effect": 0.96,
     "&:active": {
-      transform: "scale(0.96)",
+      transform: "scale(var(--pressed-effect))",
     },
   },
   variants: {
     preLarge: {
       py: 6,
       bg: "gray.100",
+      "&:active": {
+        "--pressed-effect": 0.9,
+      },
       "&:hover": {
-        transform: "scale(0.94)",
+        transform: "scale(var(--pressed-effect))",
       },
     },
   },
