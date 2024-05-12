b529a462ebd8d7cf6a34e084ed19b3d7c9e46fc3
Alex Alan Nunes <aalannunesdelima@apple.com>
2024-05-12 16:42:44 -0300

    implement alert modal

--- src/pages/saved.tsx
+++ src/pages/saved.tsx
@@ -14,7 +14,15 @@ import {
   Heading,
   Icon,
   Stack,
+  Text,
   useToast,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { BsStars } from "react-icons/bs";
 import { MdClose, MdLink } from "react-icons/md";
@@ -24,11 +32,17 @@ import { useLocalStorage } from "usehooks-ts";
 import { Header } from "../components/header";
 import { IPalette } from "../features/home/types/palette";
 import { CopiedToastContainer } from "../features/home/ui/copied-toast";
+import { useRef, useState } from "react";
 
 function SavedPaletteView() {
   const [colors, setColors] = useLocalStorage<IPalette[]>("palettes", []);
 
   const toast = useToast();
+
+  const [colorIdRemove, setColorIdRemove] = useState<null | string>(null);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef<HTMLButtonElement>(null);
 
   const handleCopy = async (value: string) => {
     await navigator.clipboard.writeText(value);
@@ -70,50 +84,89 @@ function SavedPaletteView() {
     ));
   };
 
-  const handleRemove = (colorId: string) => {
-    setColors((prev) => prev.filter((color) => color.id !== colorId));
+  const handleOpenConfirm = (colorId: string) => {
+    onOpen();
+    setColorIdRemove(colorId);
+  };
+
+  const handleRemove = () => {
+    setColors((prev) => prev.filter((color) => color.id !== colorIdRemove));
+    onClose();
+    setColorIdRemove(null);
   };
 
   return (
-    <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-      {colors.map((color) => (
-        <GridItem key={color.id} w="100%">
-          <Card maxW="sm" variant={"outline"}>
-            <CardBody>
-              <Stack spacing={4}>
-                <Flex h="40">{renderColors(color.id)}</Flex>
-                <Heading as={"h5"} fontWeight={"semibold"} fontSize={"18px"}>
-                  {color.name}
-                </Heading>
-              </Stack>
-            </CardBody>
-            <Divider borderColor={"gray.200"} />
-            <CardFooter p={2}>
-              <ButtonGroup spacing="2">
-                <Button
-                  as={Link}
-                  to={{
-                    pathname: "/",
-                    search: "colors=" + color.id.split("-").join("-"),
-                  }}
-                  leftIcon={<MdLink />}
-                >
-                  View
-                </Button>
-                <Button
-                  onClick={() => handleRemove(color.id)}
-                  variant="ghost"
-                  color={"red.300"}
-                  leftIcon={<MdClose />}
-                >
-                  Remove
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      ))}
-    </Grid>
+    <>
+      <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+        {colors.map((color) => (
+          <GridItem key={color.id} w="100%">
+            <Card maxW="sm" variant={"outline"}>
+              <CardBody>
+                <Stack spacing={4}>
+                  <Flex h="40">{renderColors(color.id)}</Flex>
+                  <Heading as={"h5"} fontWeight={"semibold"} fontSize={"18px"}>
+                    {color.name}
+                  </Heading>
+                  <Text noOfLines={1} color={"gray.600"}>
+                    {color.description}
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider borderColor={"gray.200"} />
+              <CardFooter p={2}>
+                <ButtonGroup spacing="2">
+                  <Button
+                    as={Link}
+                    to={{
+                      pathname: "/",
+                      search: "colors=" + color.id.split("-").join("-"),
+                    }}
+                    leftIcon={<MdLink />}
+                  >
+                    View
+                  </Button>
+                  <Button
+                    onClick={() => handleOpenConfirm(color.id)}
+                    variant="ghost"
+                    color={"red.300"}
+                    leftIcon={<MdClose />}
+                  >
+                    Remove
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Color
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={handleRemove} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 }
 
