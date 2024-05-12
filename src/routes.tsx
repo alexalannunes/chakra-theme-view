6b4eeffef5b6828395a0308dbb4099ae34032971
Alex Alan Nunes <aalannunesdelima@apple.com>
2024-05-12 16:16:53 -0300

    add saved page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+
 import { HomePage } from "./pages/home";
+import { SavedPalettePage } from "./pages/saved";
 
 export function RoutesApp() {
   return (
     <Routes>
       <Route index path="/" element={<HomePage />} />
+      <Route index path="/saved" element={<SavedPalettePage />} />
     </Routes>
   );
 }
